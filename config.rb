bedf5178fdd8e2e4975d69865d348600b9f563b5
Duncan Johnson <duncan@duncanandmeg.org>
2015-06-09 23:37:41 -0600

    modified config

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require "normalize-scss"
 require "susy"
 
 # Set this to the root of your project when deployed:
-http_path = "/long-haul/" 
+http_path = "/" 
 css_dir = "assets/css"
 sass_dir = "assets/scss"
 images_dir = "assets/img"
@@ -11,7 +11,7 @@ javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
